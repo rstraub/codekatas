6a6f6141e7b19d6e6c670fbea34724d21b40b518
Roy Straub <roy.straub@codesquad.nl>
2019-11-09 14:04:59 +0100

    Simplify linking

--- src/main/kotlin/io/github/rstraub/codekatas/bowlinggame/v1/Line.kt
+++ src/main/kotlin/io/github/rstraub/codekatas/bowlinggame/v1/Line.kt
@@ -12,16 +12,17 @@ class Line(scorecard: String) : ScoreProvider {
                 .split(FRAME_DELIMITER)
                 .map { Frame(it) }
 
-        frames.forEachIndexed { index, frame ->
-            var nextFrame: Frame? = null
-            if (index + 1 < frames.size)
-                nextFrame = frames[index + 1]
-
-            frame.nextFrame = nextFrame
-        }
+        linkFrames(frames)
 
         score = frames
                 .map(Frame::score)
                 .sum()
+    }
+
+    private fun linkFrames(frames: List<Frame>) {
+        frames.forEachIndexed { index, frame ->
+            if (index + 1 < frames.size)
+                frame.nextFrame = frames[index + 1]
+        }
     }
 }
