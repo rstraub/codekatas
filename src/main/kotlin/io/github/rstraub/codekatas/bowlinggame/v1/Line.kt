76d9a0c422b7a2ac0f147848b34a5efacb15f0fe
Roy Straub <roy.straub@codesquad.nl>
2019-11-09 13:26:37 +0100

    Spare implemented

--- src/main/kotlin/io/github/rstraub/codekatas/bowlinggame/v1/Line.kt
+++ src/main/kotlin/io/github/rstraub/codekatas/bowlinggame/v1/Line.kt
@@ -4,6 +4,9 @@ class Line(scorecard: String) : ScoreProvider {
     private companion object {
         private const val BALL_THROW_DELIMITER = ","
         private const val FRAME_DELIMITER = "|"
+        private const val SPARE = "/"
+        private const val ZERO = "-"
+        private const val STRIKE = "X"
     }
 
     override val score: Int
@@ -29,7 +32,7 @@ class Line(scorecard: String) : ScoreProvider {
 
     private inner class Frame(frameScore: String) : ScoreProvider {
         private val ballThrows: List<String> = frameScore.split(BALL_THROW_DELIMITER)
-        private val isSpare: Boolean = ballThrows.contains("/")
+        private val isSpare: Boolean = ballThrows.contains(SPARE)
 
         val firstThrow = ballThrows[0]
         val thirdThrow = ballThrows.getOrNull(2)
@@ -50,20 +53,22 @@ class Line(scorecard: String) : ScoreProvider {
         }
 
         private fun calculateSpareScore(): Int {
+            val bonusScore = throwScore(nextThrow())
+            return 10 + bonusScore
+        }
+
+        private fun nextThrow(): String {
             val isLastFrame = nextFrame == null
 
-            val nextThrow = if (isLastFrame) {
+            return if (isLastFrame) {
                 thirdThrow!!
             } else {
                 nextFrame!!.firstThrow
             }
-
-            val bonusScore = throwScore(nextThrow)
-            return 10 + bonusScore
         }
 
         fun throwScore(ballThrow: String) = when (ballThrow) {
-            "-" -> 0
+            ZERO -> 0
             else -> ballThrow.toInt()
         }
     }
