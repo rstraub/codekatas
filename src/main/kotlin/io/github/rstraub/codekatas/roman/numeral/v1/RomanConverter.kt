3455410fa60e90cd92caa0183eea51946a57b220
Roy Straub <roy.straub@codesquad.nl>
2019-12-23 14:57:12 +0100

    Little less ugly

--- src/main/kotlin/io/github/rstraub/codekatas/roman/numeral/v1/RomanConverter.kt
+++ src/main/kotlin/io/github/rstraub/codekatas/roman/numeral/v1/RomanConverter.kt
@@ -1,11 +1,26 @@
 package io.github.rstraub.codekatas.roman.numeral.v1
 
 class RomanConverter {
-    fun convert(number: Int) =
-        when (number) {
-            20 -> "XX"
-            10 -> "X"
-            5 -> "V"
-            else -> "I"
+    fun convert(number: Int): String {
+        var remainder = number
+        var result = ""
+        while (remainder > 0) {
+            result += when {
+                number % 10 == 0 -> {
+                    remainder -= 10
+                    "X"
+                }
+                number % 5 == 0 -> {
+                    remainder -= 5
+                    "V"
+                }
+                else -> {
+                    remainder -= 1
+                    "I"
+                }
+            }
         }
+
+        return result
+    }
 }
